9bde803abf552e257d41c7ddfb94b64196b82e48
“Yashar <yashar.hv@gmail.com>
2023-10-23 00:24:46 +0200

    Use custom S3 credentials when endpoint url exists

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 from os import getenv
 
 working_dir = getenv("WORKING_DIR")
-environment = getenv("ENV")
 video_s3_bucket = getenv("VIDEO_S3_BUCKET")
 video_s3_key = getenv("VIDEO_S3_KEY")
 s3_access_key_id = getenv("S3_ACCESS_KEY_ID")
 s3_secret_access_key = getenv("S3_SECRET_ACCESS_KEY")
 s3_endpoint_url = getenv("S3_ENDPOINT_URL")
+s3_region = getenv("S3_REGION")
--- src/services/s3_service.py
+++ src/services/s3_service.py
@@ -1,14 +1,14 @@
 import boto3
 import os
-from config import s3_access_key_id, s3_secret_access_key, s3_endpoint_url, working_dir, environment
+from config import s3_access_key_id, s3_secret_access_key, s3_endpoint_url, working_dir, s3_region
 from utils import logger
 
 
-if environment == "localhost":
-    s3_client = boto3.client(service_name="s3", region_name="eu-west-1", aws_access_key_id=s3_access_key_id,
+if s3_endpoint_url:
+    s3_client = boto3.client(service_name="s3", region_name=s3_region, aws_access_key_id=s3_access_key_id,
                              aws_secret_access_key=s3_secret_access_key, endpoint_url=s3_endpoint_url)
 else:
-    s3_client = boto3.client(service_name="s3", region_name="eu-west-1")
+    s3_client = boto3.client(service_name="s3", region_name=s3_region)
 
 
 def download_video(bucket, video_key):
@@ -24,7 +24,6 @@ def download_video(bucket, video_key):
         return local_file_path
 
     except Exception as error:
-        # Put your error handling logic here
         logger.log_error('S3: {}'.format(error))
 
 
