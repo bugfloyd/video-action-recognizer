61a30bbff56cc70bc0cd9cc4e2bff8e3f88dc28b
“Yashar <yashar.hv@gmail.com>
2023-11-27 03:28:48 +0100

    Add basic API Gateway infra

--- infrastructure/uploader_lambda.tf
+++ infrastructure/uploader_lambda.tf
@@ -11,12 +11,7 @@ resource "aws_lambda_function" "uploader_lambda" {
 
   # environment {
   #   variables = {
-  #     ANALYSIS_CORE_ECS_TASK_DEFINITION = aws_ecs_task_definition.analysis_core_task.arn
-  #     ANALYSIS_CORE_ECS_CLUSTER         = aws_ecs_cluster.analysis_core_cluster.name
-  #     ANALYSIS_CORE_SUBNET_ID_1         = aws_subnet.private_subnet_az1.id
-  #     ANALYSIS_CORE_SUBNET_ID_2         = aws_subnet.private_subnet_az2.id
-  #     ANALYSIS_CORE_SECURITY_GROUP      = aws_security_group.analysis_core_sg.id
-  #     ANALYSIS_CORE_CONTAINER_NAME      = local.analysis_core_container_name
+  #     AENV_VAR_NAME = "VAR_VALUE"
   #   }
   # }
 }
@@ -80,4 +75,57 @@ resource "aws_iam_role_policy_attachment" "uploader_lambda_logs_attachment" {
 resource "aws_iam_role_policy_attachment" "uploader_lambda_task_attachment" {
   role       = aws_iam_role.uploader_lambda_exec_role.name
   policy_arn = aws_iam_policy.uploader_lambda_task_policy.arn
+}
+
+
+resource "aws_api_gateway_rest_api" "uploader_api_gateway" {
+  name = "UploadAPI"
+}
+
+resource "aws_api_gateway_resource" "uploader_resource" {
+  rest_api_id = aws_api_gateway_rest_api.uploader_api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.uploader_api_gateway.root_resource_id
+  path_part   = "upload"
+}
+
+resource "aws_api_gateway_method" "uploader_method" {
+  rest_api_id   = aws_api_gateway_rest_api.uploader_api_gateway.id
+  resource_id   = aws_api_gateway_resource.uploader_resource.id
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.uploader_cognito_authorizer.id
+}
+
+resource "aws_api_gateway_integration" "uploader_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.uploader_api_gateway.id
+  resource_id             = aws_api_gateway_resource.uploader_resource.id
+  http_method             = "GET"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.uploader_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "api_deployment" {
+  depends_on = [aws_api_gateway_integration.uploader_lambda_integration]
+
+  rest_api_id = aws_api_gateway_rest_api.uploader_api_gateway.id
+  stage_name  = "prod"
+}
+
+resource "aws_api_gateway_authorizer" "uploader_cognito_authorizer" {
+  name            = "UploaderCognitoAuthorizer"
+  rest_api_id     = aws_api_gateway_rest_api.uploader_api_gateway.id
+  type            = "COGNITO_USER_POOLS"
+  identity_source = "method.request.header.Authorization"
+  provider_arns   = [aws_cognito_user_pool.main.arn]
+}
+
+resource "aws_lambda_permission" "api_lambda_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.uploader_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # Depends on the API Gateway deployment to exist before permission is granted
+  source_arn = "${aws_api_gateway_rest_api.uploader_api_gateway.execution_arn}/*/*"
 }
