0a46da773ba37114827066f9faa257082d0f3c3f
“Yashar <yashar.hv@gmail.com>
2023-12-02 21:17:34 +0100

    Add IAM role and provider for GitHub workflow

---
+++ infrastructure/setup/pipeline_exec.tf
@@ -0,0 +1,43 @@
+# Create an IAM OIDC identity provider for GitHub Actions
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
+}
+
+# Create an IAM role for GitHub Actions
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com",
+          },
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" : "repo:${var.github_repo}:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Attach policies to the IAM role as needed
+# Example: attaching a read-only policy
+resource "aws_iam_role_policy_attachment" "administrator_access" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "pipeline_execution_role_arn" {
+  value = aws_iam_role.github_actions.arn
+}
--- infrastructure/setup/variables.tf
+++ infrastructure/setup/variables.tf
@@ -24,3 +24,8 @@ variable "lambda_bucket" {
   description = "The AWS S3 bucket for storing lambda function codes"
   type        = string
 }
+
+variable "github_repo" {
+  description = "Github repo to be used to allow github actions to have access to the AWS account via OIDC in this format: <GITHUB_OWNER>/<GITHUB_REPO>"
+  type        = string
+}
