138c65c3fce650754c132f72128d911d9677d479
“Yashar <yashar.hv@gmail.com>
2023-12-08 00:27:01 +0100

    Add Results lambda function code

--- rest-backend/results/src/index.ts
+++ rest-backend/results/src/index.ts
@@ -3,14 +3,14 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } f
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const httpMethod = event.httpMethod;
     const pathParameters = event.pathParameters;
-    const userId = pathParameters ? pathParameters['user_id'] : null;
+    const resultId = pathParameters ? pathParameters['result_id'] : null;
 
     return {
         statusCode: 200,
         body: JSON.stringify({
             message: 'Request processed successfully',
             method: httpMethod,
-            userId
+            resultId
         }),
     };
 };
