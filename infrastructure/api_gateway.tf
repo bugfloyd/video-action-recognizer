61ebda3d0eceb45d226bddb9d0084b5e0e1c6356
“Yashar <yashar.hv@gmail.com>
2023-12-08 00:42:34 +0100

    Remove unused upload_request_lambda_bundle_sha var

--- infrastructure/api_gateway.tf
+++ infrastructure/api_gateway.tf
@@ -5,15 +5,14 @@ resource "aws_api_gateway_rest_api" "var_backend" {
 module "rest_backend" {
   source = "./rest_backend"
 
-  aws_region                       = var.aws_region
-  lambda_bucket                    = var.lambda_bucket
-  upload_request_lambda_bundle_sha = var.upload_request_lambda_bundle_sha
-  api_gateway_execution_arn        = "${aws_api_gateway_rest_api.var_backend.execution_arn}/*/*"
-  rest_api_id                      = aws_api_gateway_rest_api.var_backend.id
-  api_root_resource_id             = aws_api_gateway_rest_api.var_backend.root_resource_id
-  authorizer_id                    = aws_api_gateway_authorizer.var_cognito_authorizer.id
-  users_lambda_bundle_sha          = var.users_lambda_bundle_sha
-  user_pool_id                     = aws_cognito_user_pool.main.id
+  aws_region                = var.aws_region
+  lambda_bucket             = var.lambda_bucket
+  api_gateway_execution_arn = "${aws_api_gateway_rest_api.var_backend.execution_arn}/*/*"
+  rest_api_id               = aws_api_gateway_rest_api.var_backend.id
+  api_root_resource_id      = aws_api_gateway_rest_api.var_backend.root_resource_id
+  authorizer_id             = aws_api_gateway_authorizer.var_cognito_authorizer.id
+  users_lambda_bundle_sha   = var.users_lambda_bundle_sha
+  user_pool_id              = aws_cognito_user_pool.main.id
 }
 
 resource "aws_api_gateway_deployment" "api_deployment" {
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -23,11 +23,6 @@ variable "upload_listener_lambda_bundle_sha" {
   type        = string
 }
 
-variable "upload_request_lambda_bundle_sha" {
-  description = "Uploader lambda function zip sha to be used in source_code_hash"
-  type        = string
-}
-
 variable "users_lambda_bundle_sha" {
   description = "Create user lambda function zip sha to be used in source_code_hash"
   type        = string
