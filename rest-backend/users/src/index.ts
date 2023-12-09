871f2b1c5846c96535bf221ded38e65f167c7dae
“Yashar <yashar.hv@gmail.com>
2023-12-09 16:55:18 +0100

    Install Prettier & ESLint to Users lambda

--- rest-backend/users/src/index.ts
+++ rest-backend/users/src/index.ts
@@ -1,16 +1,22 @@
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult,
+} from 'aws-lambda'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const httpMethod = event.httpMethod;
-    const pathParameters = event.pathParameters;
-    const userId = pathParameters ? pathParameters['user_id'] : null;
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const httpMethod = event.httpMethod
+  const pathParameters = event.pathParameters
+  const userId = pathParameters ? pathParameters['user_id'] : null
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Request processed successfully',
-            method: httpMethod,
-            userId
-        }),
-    };
-};
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Request processed successfully',
+      method: httpMethod,
+      userId,
+    }),
+  }
+}
