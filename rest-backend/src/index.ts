087fc549c0b2e97044ac9a01163a6441e825eaff
Yashar Hosseinpour <yashar.hv@gmail.com>
2024-03-30 18:18:14 +0100

    Fix orphaned import line

--- rest-backend/src/index.ts
+++ rest-backend/src/index.ts
@@ -5,7 +5,6 @@ import { ServiceRouter } from './types/types';
 import { awsRegion } from './variables';
 import { globalCases } from './exceptions/cases/globalCases';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import 'reflect-metadata';
 
 const usersRouter: ServiceRouter = async (event) => {
   if (!userPoolId) {
