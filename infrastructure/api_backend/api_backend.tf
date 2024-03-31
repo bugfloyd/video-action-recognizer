3d40c3a68974d98b2b73be3f3c70f6f78d6213ec
Yashar Hosseinpour <yashar.hv@gmail.com>
2024-03-31 18:52:56 +0200

    Add file entity
    Add file-related endpoints to API Gateway infra
    Add create file endpoint and the related abstraction layers:
    - model
    - repository
    - types
    - service
    - cases
    - controller
    - router
    
    Install dynamoose uuid

--- infrastructure/api_backend/api_backend.tf
+++ infrastructure/api_backend/api_backend.tf
@@ -69,7 +69,19 @@ resource "aws_api_gateway_resource" "users_resource" {
 resource "aws_api_gateway_resource" "user_resource" {
   rest_api_id = aws_api_gateway_rest_api.var_rest_backend.id
   parent_id   = aws_api_gateway_resource.users_resource.id
-  path_part   = "{user_id}"
+  path_part   = "{userId}"
+}
+
+resource "aws_api_gateway_resource" "files_resource" {
+  rest_api_id = aws_api_gateway_rest_api.var_rest_backend.id
+  parent_id   = aws_api_gateway_rest_api.var_rest_backend.root_resource_id
+  path_part   = "files"
+}
+
+resource "aws_api_gateway_resource" "file_resource" {
+  rest_api_id = aws_api_gateway_rest_api.var_rest_backend.id
+  parent_id   = aws_api_gateway_resource.files_resource.id
+  path_part   = "{fileId}"
 }
 
 # API Gateway http methods
@@ -108,6 +120,46 @@ resource "aws_api_gateway_method" "patch_user" {
 resource "aws_api_gateway_method" "delete_user" {
   rest_api_id   = aws_api_gateway_rest_api.var_rest_backend.id
   resource_id   = aws_api_gateway_resource.user_resource.id
+  http_method   = "DELETE"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.var_cognito_authorizer.id
+}
+
+resource "aws_api_gateway_method" "post_file" {
+  rest_api_id   = aws_api_gateway_rest_api.var_rest_backend.id
+  resource_id   = aws_api_gateway_resource.files_resource.id
+  http_method   = "POST"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.var_cognito_authorizer.id
+}
+
+resource "aws_api_gateway_method" "get_files" {
+  rest_api_id   = aws_api_gateway_rest_api.var_rest_backend.id
+  resource_id   = aws_api_gateway_resource.files_resource.id
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.var_cognito_authorizer.id
+}
+
+resource "aws_api_gateway_method" "get_file" {
+  rest_api_id   = aws_api_gateway_rest_api.var_rest_backend.id
+  resource_id   = aws_api_gateway_resource.file_resource.id
+  http_method   = "GET"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.var_cognito_authorizer.id
+}
+
+resource "aws_api_gateway_method" "patch_file" {
+  rest_api_id   = aws_api_gateway_rest_api.var_rest_backend.id
+  resource_id   = aws_api_gateway_resource.file_resource.id
+  http_method   = "PATCH"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.var_cognito_authorizer.id
+}
+
+resource "aws_api_gateway_method" "delete_file" {
+  rest_api_id   = aws_api_gateway_rest_api.var_rest_backend.id
+  resource_id   = aws_api_gateway_resource.file_resource.id
   http_method   = "DELETE"
   authorization = "COGNITO_USER_POOLS"
   authorizer_id = aws_api_gateway_authorizer.var_cognito_authorizer.id
@@ -154,6 +206,51 @@ resource "aws_api_gateway_integration" "delete_user_lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.var_rest_backend.id
   resource_id             = aws_api_gateway_resource.user_resource.id
   http_method             = aws_api_gateway_method.delete_user.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.rest_backend_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "get_files_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.var_rest_backend.id
+  resource_id             = aws_api_gateway_resource.files_resource.id
+  http_method             = aws_api_gateway_method.get_files.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.rest_backend_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "post_file_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.var_rest_backend.id
+  resource_id             = aws_api_gateway_resource.files_resource.id
+  http_method             = aws_api_gateway_method.post_file.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.rest_backend_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "get_file_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.var_rest_backend.id
+  resource_id             = aws_api_gateway_resource.file_resource.id
+  http_method             = aws_api_gateway_method.get_file.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.rest_backend_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "patch_file_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.var_rest_backend.id
+  resource_id             = aws_api_gateway_resource.file_resource.id
+  http_method             = aws_api_gateway_method.patch_file.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.rest_backend_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "delete_file_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.var_rest_backend.id
+  resource_id             = aws_api_gateway_resource.file_resource.id
+  http_method             = aws_api_gateway_method.delete_file.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.rest_backend_lambda.invoke_arn
